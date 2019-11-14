5ec4b020e85e39b316fd0bc3276ae77609a6d020
Mat <mat555333@gmail.com>
2019-11-14 13:32:06 +0100

    terrible code for terrible people

--- Assets/Scenes/walktest.cs
+++ Assets/Scenes/walktest.cs
@@ -5,19 +5,21 @@ using UnityEngine;
 public class walktest : MonoBehaviour
 {
     private SpriteRenderer sprite;
+    private bool facingRight = false;
+    private bool directionChanged = false;
     // Start is called before the first frame update
     void Start()
     {
         sprite = GetComponent<SpriteRenderer>();
-        Vector2 goal = new Vector2(20, -56); // 56 is because of the offset of the sprite
+        Vector2 goal = new Vector2(-20, -56); // 56 is because of the offset of the sprite
         Vector2 intermediateGoal;
-
+        
 
 
         TickSystem.OnTick += delegate (object sender, TickSystem.OnTickEventArgs e)
         {
             // are we at our goal?
-            if (transform.position.x == goal.x)
+            if (transform.position.x == goal.x && transform.position.y == goal.y)
             {
 
             }
@@ -28,6 +30,7 @@ public class walktest : MonoBehaviour
                 if (goal.y != transform.position.y)
                 {
                     // set intermediate goal to be door of current level
+                    // should be using door coords instead but it works for now
                     intermediateGoal = new Vector2(0, transform.position.y);
                 }
                 // if goal is on same level as us
@@ -49,25 +52,46 @@ public class walktest : MonoBehaviour
 
     private void WalkTo(float locationX)
     {
+        // heading right
+        if (transform.position.x < locationX)
+        {
+            facingRight = false;
+        }
+        // heading left
+        if (transform.position.x > locationX)
+        {
+            facingRight = true;
+        }
+        if (facingRight != directionChanged)
+        {
+            directionChanged = facingRight;
+            FlipSprite();
+        }
         // walk to 0
         if (transform.position.x != locationX)
         {
-            // walk right to 0
-            if (transform.position.x < locationX)
+            if (facingRight == false)
             {
                 gameObject.GetComponent<SpriteRenderer>().sprite = GameObject.Find("GameManager").GetComponent<AnimationHandler>().Minek(gameObject.GetComponent<SpriteRenderer>().sprite);
                 transform.position = new Vector2(transform.position.x + 1, transform.position.y);
             }
-            // walk left to 0
-            if (transform.position.x > locationX)
+            if (facingRight == true)
             {
                 gameObject.GetComponent<SpriteRenderer>().sprite = GameObject.Find("GameManager").GetComponent<AnimationHandler>().Minek(gameObject.GetComponent<SpriteRenderer>().sprite);
                 transform.position = new Vector2(transform.position.x - 1, transform.position.y);
             }
         }
     }
-}
+    private void FlipSprite()
+    {
+        sprite.flipX = !sprite.flipX;
+        if (sprite.flipX == true)
+        {
+            transform.position = new Vector2(transform.position.x + 19, transform.position.y);
+        }
+    }
 
+}
 
 
 
